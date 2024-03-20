6a95c118d74a64fe92bc6a2d47d4a087051f83f7
spatilbroad <spatil@broadinstitute.org>
2024-03-20 11:27:20 -0400

    iterate

--- queuelist.go
+++ queuelist.go
@@ -9,6 +9,7 @@ type Host struct {
 	XMLName       xml.Name     `xml:"Queue-List" json:"-"`
 	Name          string       `xml:"name" json:"name"`
 	QType         string       `xml:"qtype" json:"qtype"`
+//added the state below	
 	State		  string       `xml:"state" json:"state"`
 	SlotsUsed     int32        `xml:"slots_used" json:"slots_used"`
 	SlotsReserved int32        `xml:"slots_rsv" json:"slots_reserved"`
